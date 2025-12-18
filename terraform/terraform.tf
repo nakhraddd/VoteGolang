7b6f9a35e43bda03f4c40356a6b70caac913334c
darkhan <d_tastanov@kbtu.kz>
2025-12-18 22:01:14 +0500

    import

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,18 @@
+import {
+  id = "projects/${var.project_id}/global/firewalls/allow-http-ssh-kafka"
+  to = google_compute_firewall.default
+}
+
+import {
+  id = "projects/${var.project_id}/zones/europe-west4-a/instances/votegolang-vm"
+  to = google_compute_instance.app_server
+}
+
+import {
+  id = "projects/${var.project_id}/regions/europe-west4/addresses/votegolang-static-ip"
+  to = google_compute_address.static_ip
+}
+
 terraform {
   required_providers {
     google = {
@@ -89,9 +104,4 @@ resource "google_compute_instance" "app_server" {
 
 output "instance_ip" {
   value = google_compute_address.static_ip.address
-}
-
-import {
-  id = "projects/${var.project_id}/regions/europe-west4/addresses/votegolang-static-ip"
-  to = google_compute_address.static_ip
 }
