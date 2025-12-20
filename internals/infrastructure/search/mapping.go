46c56c418caa12b8663fa3413182f2197436e209
darkhan <d_tastanov@kbtu.kz>
2025-12-20 18:25:32 +0500

    for searchbar

--- internals/infrastructure/search/mapping.go
+++ internals/infrastructure/search/mapping.go
@@ -15,7 +15,7 @@ const CandidateMapping = `
       "tokenizer": {
         "edge_ngram_tokenizer": {
           "type": "edge_ngram",
-          "min_gram": 2,
+          "min_gram": 1,
           "max_gram": 15,
           "token_chars": [
             "letter",
@@ -44,10 +44,35 @@ const CandidateMapping = `
 
 const PetitionMapping = `
 {
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "edge_ngram_analyzer": {
+          "tokenizer": "edge_ngram_tokenizer",
+          "filter": [
+            "lowercase"
+          ]
+        }
+      },
+      "tokenizer": {
+        "edge_ngram_tokenizer": {
+          "type": "edge_ngram",
+          "min_gram": 1,
+          "max_gram": 15,
+          "token_chars": [
+            "letter",
+            "digit"
+          ]
+        }
+      }
+    }
+  },
   "mappings": {
     "properties": {
       "title": {
-        "type": "text"
+        "type": "text",
+        "analyzer": "edge_ngram_analyzer",
+        "search_analyzer": "standard"
       },
       "description": {
         "type": "text"
