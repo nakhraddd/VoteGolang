285925e61849e9f68be0d5d83e078e9b5905ccd2
darkhan <d_tastanov@kbtu.kz>
2025-12-20 18:06:49 +0500

    fixing indexes

--- internals/domain/candidate.go
+++ internals/domain/candidate.go
@@ -8,20 +8,20 @@ import (
 
 // Candidate represents a candidate for election.
 type Candidate struct {
-	ID             uint           `gorm:"primaryKey;autoIncrement" swaggerignore:"true"`
-	Name           string         `gorm:"type:varchar(255);not null" example:"Beksultan"`
-	Photo          *string        `gorm:"type:varchar(255)" example:"link"`
-	Education      *string        `gorm:"type:varchar(255)" example:"KBTU"`
-	Age            int            `gorm:"not null" example:"20"`
-	Party          *string        `gorm:"type:varchar(255)" example:"Jastar"`
-	Region         *string        `gorm:"type:varchar(255)" example:"SKO"`
-	Votes          int            `gorm:"default:0" swaggerignore:"true"`
-	Type           CandidateType  `gorm:"type:varchar(255);not null" example:"manager"`
+	ID             uint           `gorm:"primaryKey;autoIncrement" swaggerignore:"true" json:"id"`
+	Name           string         `gorm:"type:varchar(255);not null" example:"Beksultan" json:"name"`
+	Photo          *string        `gorm:"type:varchar(255)" example:"link" json:"photo"`
+	Education      *string        `gorm:"type:varchar(255)" example:"KBTU" json:"education"`
+	Age            int            `gorm:"not null" example:"20" json:"age"`
+	Party          *string        `gorm:"type:varchar(255)" example:"Jastar" json:"party"`
+	Region         *string        `gorm:"type:varchar(255)" example:"SKO" json:"region"`
+	Votes          int            `gorm:"default:0" swaggerignore:"true" json:"votes"`
+	Type           CandidateType  `gorm:"type:varchar(255);not null" example:"manager" json:"type"`
 	VotingStart    time.Time      `json:"voting_start" gorm:"type:datetime" example:"2025-11-12T09:00:00+05:00"`
 	VotingDeadline time.Time      `json:"voting_deadline" gorm:"type:datetime" example:"2026-11-12T09:00:00+05:00"`
 	DeletedAt      gorm.DeletedAt `json:"-" swaggerignore:"true"`
-	CreatedAt      time.Time      `gorm:"autoCreateTime" swaggerignore:"true"`
-	UpdatedAt      time.Time      `gorm:"autoUpdateTime" swaggerignore:"true"`
+	CreatedAt      time.Time      `gorm:"autoCreateTime" swaggerignore:"true" json:"created_at"`
+	UpdatedAt      time.Time      `gorm:"autoUpdateTime" swaggerignore:"true" json:"updated_at"`
 }
 
 // CandidateRepository retrieves candidate data.
--- internals/domain/petition.go
+++ internals/domain/petition.go
@@ -8,18 +8,18 @@ import (
 
 // Petition represents a created petition.
 type Petition struct {
-	ID             uint           `gorm:"primaryKey;autoIncrement" swaggerignore:"true"`
-	UserID         uint           `gorm:"not null" swaggerignore:"true"`
-	Title          string         `gorm:"type:varchar(255);not null" example:"petition title"`
-	Photo          *string        `gorm:"type:varchar(255)" example:"link"`
-	Description    *string        `gorm:"type:text" example:"petition description"`
-	VotesInFavor   int            `gorm:"default:0" swaggerignore:"true"`
-	VotesAgainst   int            `gorm:"default:0" swaggerignore:"true"`
-	Goal           int            `gorm:"not null" example:"0"`
+	ID             uint           `gorm:"primaryKey;autoIncrement" swaggerignore:"true" json:"id"`
+	UserID         uint           `gorm:"not null" swaggerignore:"true" json:"user_id"`
+	Title          string         `gorm:"type:varchar(255);not null" example:"petition title" json:"title"`
+	Photo          *string        `gorm:"type:varchar(255)" example:"link" json:"photo"`
+	Description    *string        `gorm:"type:text" example:"petition description" json:"description"`
+	VotesInFavor   int            `gorm:"default:0" swaggerignore:"true" json:"votes_in_favor"`
+	VotesAgainst   int            `gorm:"default:0" swaggerignore:"true" json:"votes_against"`
+	Goal           int            `gorm:"not null" example:"0" json:"goal"`
 	VotingDeadline time.Time      `json:"voting_deadline" gorm:"type:datetime" example:"2025-05-10T23:59:00+05:00"`
 	DeletedAt      gorm.DeletedAt `json:"-" swaggerignore:"true"`
-	CreatedAt      time.Time      `gorm:"autoCreateTime" swaggerignore:"true"`
-	UpdatedAt      time.Time      `gorm:"autoUpdateTime" swaggerignore:"true"`
+	CreatedAt      time.Time      `gorm:"autoCreateTime" swaggerignore:"true" json:"created_at"`
+	UpdatedAt      time.Time      `gorm:"autoUpdateTime" swaggerignore:"true" json:"updated_at"`
 }
 
 type PetitionRepository interface {
